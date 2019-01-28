afdfe066895bdb3a6023cdc89a7772d30dea0e7c
Darius Ndubi <ndubidarius@gmail.com>
2019-01-28 09:35:10 +0300

    Add home page Add register Add login Add about us page

---
+++ src/features/articles/ArticleList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import NavBar from '../nav/navBar/NavBar';
+
+class ArticleList extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+      </div>
+    )
+  }
+}
+
+export default ArticleList;
