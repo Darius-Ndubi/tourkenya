497d5ba5bc25a1905169665bf464cd1c94fd0ca2
Darius Ndubi <ndubidarius@gmail.com>
2019-02-03 23:07:08 +0300

    Fix login routing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // send the user to index html page inspite of the url
 app.get('/*', function (req, res) {
-  res.sendFile(__dirname, 'dist', 'index.html');
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
 
